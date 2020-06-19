c31fdda5524e550224a9d04f2713c0d6e216fa87
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-06-19 18:45:48 -0500

    added some notes and made small adjustments. still need to update images to pull from AWS.

--- prrget/src/Searchbar.js
+++ prrget/src/Searchbar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 
-const Searchbar = ({getSearchedCat, catChange, cats}) => {
+const Searchbar = ({searchDropdown, getSearchedCat, catChange, cats}) => {
+
   return (
 
-    <div className="searchBar">
+    <div className="navWrapper">
       <nav className="navBar">
-        <li>
+
+        <li className="targetLogo">
+          {/* TARGET LOGO IMAGE */}
           <img className="targetLogo"
             src="https://imgur.com/DXdvZDr.jpg"
             alt="target logo"
           ></img>
         </li>
 
+            {/* CATEGORY DROP DOWN */}
+      
         <li className="category">
           <select className="category">
             <option>Categories</option>
@@ -21,43 +26,48 @@ const Searchbar = ({getSearchedCat, catChange, cats}) => {
             <option>Hairless</option>
             <option>Adorable</option>
           </select>
-        </li>
-
-        <li className="sameDayDelivery">
+            {/* SAME DAY DELIVERY BUTTON */}
+        
           <button className="sameDayDelivery">same day delivery</button>
         </li>
 
-        <li className="search">
+            {/* SEARCH BAR */}
+        <li className="searchBarWrap">
           <form className="form" onSubmit={(event) => getSearchedCat(event)}>
             <input
+              onClick={() => searchDropdown()}
+              autoCorrect="off"
+              autoComplete="off"
+              className="search"
               onChange={catChange}
-              type="text" className="search"
+              type="search"
+              id="search"
               placeholder="Search"
               name="catName"
               value={cats}
+              aria-label="Search: suggestions appear below"
             />
+            <span className="searchIcon">
+              <span className="glyphicon glyphicon-search"/>
+            </span>
           </form>
-        </li>
-
-        <li>
-
-          <img className="userIcon"
-            src="https://imgur.com/J3UlNAD.jpg"
-            alt="users icon"
-          >
-          </img>
-        </li>
-
-        <li>
-          <button className="shoppingCart">
-            <img className="shoppingCart"
-                  src="https://imgur.com/nZ0q7ht.jpg"
-                  alt="shopping cart"
+                  {/* user icon image */}
+          <span className="userIcon">
+            <img className="userIcon"
+              src="https://imgur.com/J3UlNAD.jpg"
+              alt="users icon"
             >
             </img>
-          </button>
+          </span>
+                 {/* shopping cart image */}
+          <span className="shoppingCart">
+              <img className="shoppingCart"
+                    src="https://imgur.com/nZ0q7ht.jpg"
+                    alt="shopping cart"
+              >
+              </img>
+          </span>
         </li>
-
       </nav>
     </div>
   )
