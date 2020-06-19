3d98767ca8054db3f49e0dbab88891669308a971
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-06-19 18:47:54 -0500

    added render and click feature for the search drop down

--- prrget/src/App.js
+++ prrget/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Searchbar from "./Searchbar.js";
+import SearchDropdown from "./SearchDropdown.js";
 
 const axios = require('axios');
 
@@ -9,11 +10,14 @@ class App extends React.Component {
     super();
     this.state = {
       cats: [],
-      catName: ''
+      catName: 'test',
+      searchDrop: false
     };
     this.getCat = this.getCat.bind(this);
     this.onCatNameChange = this.onCatNameChange.bind(this);
     this.getSearchedCat = this.getSearchedCat.bind(this);
+    this.searchDropAnimation = this.searchDropAnimation.bind(this);
+    this.searchDropFade = this.searchDropFade.bind(this);
   }
   componentDidMount () {
     this.getCat();
@@ -21,9 +25,10 @@ class App extends React.Component {
 
   }
 
-  getCat () {
-    console.log(this.state.catName)
-    axios.get(`http://192.168.1.70:5300/cats/${this.state.catName}`)
+  getCat (event) {
+    var catName = this.state.catName;
+    console.log('get request cat name: ', catName);
+    axios.get(`/cats/${catName}`)
       .then(res => {
         this.setState({
           cats: res.data
@@ -50,18 +55,36 @@ class App extends React.Component {
     })
   }
 
+  searchDropAnimation (event) {
+    setTimeout(() => {
+    this.setState ({
+      searchDrop: true
+    })}, 200);
+  }
+
+  searchDropFade (event) {
+    this.setState ({
+      searchDrop: false
+    })
+  }
+
   render() {
 
     var renderedCat = 'NO KITTY'
+    var renderSearchDrop = '';
 
     if(this.state.cats.length !== 0) {
       console.log(this.state.cats)
       renderedCat = this.state.cats[0].name
     }
+    if(this.state.searchDrop === true) {
+      renderSearchDrop = <SearchDropdown searchDrop={this.searchDropFade} />
+    }
     return (
       <div>
       <div>
         <Searchbar
+          searchDropdown={this.searchDropAnimation}
           getSearchedCat={this.getSearchedCat}
           getCat={this.getCat}
           catChange={this.onCatNameChange}
@@ -69,6 +92,7 @@ class App extends React.Component {
       </div>
 
       <div className="testCat">Test, Cat from Database: {renderedCat}  </div>
+      <div>{renderSearchDrop}</div>
 
       </div>
     );
