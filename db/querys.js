cc6df7d7ecaf32b35f8617e63a08cd7126985960
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-06-29 11:30:20 -0500

    added query stuff

--- db/querys.js
+++ db/querys.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: process.env.RDS_HOSTNAME || 'localhost',
   user: process.env.RDS_USERNAME || 'root',
   password: process.env.RDS_PASSWORD || 'password123',
-  database: process.env.RDS_DB_NAME || 'prrget',
+  database: 'prrget',
   port: process.env.RDS_HOSTNAME || 3306
 } );
 
