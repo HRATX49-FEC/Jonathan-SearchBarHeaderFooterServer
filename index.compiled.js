7eeae3b06633797bc9c9abf61c6cd54f410dafe8
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-07-01 10:02:22 -0500

    added compiled server file

---
+++ index.compiled.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+
+var path = require('path');
+
+var PORT = process.env.PORT || 5300;
+
+var query = require('./db/querys.js');
+
+var bodyparser = require('body-parser');
+
+app.use(express["static"](path.join(__dirname, './client/dist/')));
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+app.use(bodyparser.json());
+app.get("/search/:catName", function (req, res) {
+  var params = [req.params.catName];
+  console.log(params);
+  query.getCats(params, function (err, results) {
+    if (err) {
+      console.log('error getting from server: ', err);
+      res.status(504);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+app.get("/search/cart/cats", function (req, res) {
+  query.getCart(function (err, results) {
+    if (err) {
+      console.log('error getting from server: ', err);
+      res.status(504);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+app.post("/search/cart/delete/post", function (req, res) {
+  params = [req.body.catName, req.body.price];
+  console.log('post to cat cart: ', params);
+  query.postCatToCart(params, function (err, results) {
+    if (err) {
+      console.log('error getting from server: ', err);
+      res.status(504);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+app.get("/search/cart/delete/:catId", function (req, res) {
+  params = [req.params.catId];
+  console.log(params);
+  query.deleteCatFromCart(params, function (err, results) {
+    if (err) {
+      console.log('error deleting cat from cart on server: ', err);
+      res.status(504);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+app.listen(PORT, function () {
+  console.log("connected to port: ".concat(PORT));
+});
